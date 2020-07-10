23738ba5a2d3caaebe17dccdd65f9ec717e538c0
adrianolmedo <adrianolmedo.ve@gmail.com>
2020-07-10 13:44:33 -0400

    Fixed bug: banners is not displaying in categories assigneds and updated version

--- banners/index.php
+++ banners/index.php
@@ -27,7 +27,7 @@
 Plugin Name: XenoTrue Online Banners
 Plugin URI: https://gitlab.com/xenotrue/development/osclass_plug_xt-banners
 Description: Put banners on the site, in any format and check its display time
-Version: 0.1-beta
+Version: 0.2-beta
 Author: XenoTrue
 Author URI: https://gitlab.com/xenotrue
 Short Name: banners
