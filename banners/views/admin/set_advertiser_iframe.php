fe735c3c0c7c039eafcbf3f60dc3f2d4fac54de1
Adrián Olmedo <adrianolmedo.ve@gmail.com>
2021-10-02 18:05:26 -0400

    Updated LICENSE

--- banners/views/admin/set_advertiser_iframe.php
+++ banners/views/admin/set_advertiser_iframe.php
@@ -3,7 +3,7 @@
     /*
      * MIT License
      * 
-     * Copyright (c) 2020 XenoTrue
+     * Copyright (c) 2021 CodexiLab
      * 
      * Permission is hereby granted, free of charge, to any person obtaining a copy
      * of this software and associated documentation files (the "Software"), to deal
